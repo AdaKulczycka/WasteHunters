a17ebcd4228ec5d07610779414791c405098668d
Vlad <vlad.tomulescu317@gmail.com>
2024-10-25 21:14:07 +0200

    yea

--- WorldOfZuul/obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
+++ WorldOfZuul/obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813eae15f9c72d55851b131c99de86a87c3862e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd22b5effdc4bdaba700fba6ab6fddb659340ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
