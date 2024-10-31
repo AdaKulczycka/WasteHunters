dc1c07e8dbfda8d6b52a8175d9df13cb02658cca
Vlad <vlad.tomulescu317@gmail.com>
2024-10-31 22:14:33 +0100

    comment

--- WorldOfZuul/Game.cs
+++ WorldOfZuul/Game.cs
@@ -24,7 +24,7 @@ namespace WorldOfZuul
             // currentRoom = bathroom;
             // trash = new List<string>{currentRoom.Trash[random.Next(0, currentRoom.Trash.Count())]};
 
-            Room? hallway = new("Hallway", "You are standing in the hallway of your little cozy home.\n To the north you see your bathroom.\n To the east you can exit your house to the City Center.\n To the south there is... nothing.\n To the west you see your kitchen.", new List<string>{"pet food leftovers", "shattered glass", "old torn shoelaces"});
+             Room? hallway = new("Hallway", "You are standing in the hallway of your little cozy home.\n To the north you see your bathroom.\n To the east you can exit your house to the City Center.\n To the south there is... nothing.\n To the west you see your kitchen.", new List<string>{"pet food leftovers", "shattered glass", "old torn shoelaces"});
             Room? bathroom = new("Bathroom", "You are in the bathroom.\n To the north there is... nothing.\n To the east there is... nothing.\n To the south you see your hallway.\n To the west you can see your bedroom.", new List<string>{ "cotton buds", "used razor blades", "empty tube of toothpaste", "empty roll of toilet paper" });
             Room? bedroom = new("Bedroom", "You are in the bedroom.\n To the north there is... nothing.\n To the east you see your bathroom.\n To the south you see your kitchen.\n To the west there is... nothing." ,new List<string>{ "pair of socks with holes", "candy wrapper", "not working computer mouse", "used batteries" });
             Room? kitchen = new("Kitchen", "You are in the kitchen.\n To the north you see your bedroom.\n To the east you see your hallway.\n To the south you see your living room.\n To the west there is... nothing." ,new List<string>{ "egg shells", "rotten banana", "empty milk carton", "empty glass jar of pesto", "empty can of animal food" });
@@ -41,7 +41,26 @@ namespace WorldOfZuul
             Room? dumpingYard = new("Dumping yard", "You've entered a dumping yard.\n To the north you see a clearing.\n To the east you see a pond.\n To the south you see a museum.\n To the west you see a beach." ,new List<string>{"Nothing to see here"}); //Implement except this location, there is nothing to pick up here
             Room? school = new("School", "You've entered a school.\n To the north you see a museum.\n To the east you see a park.\n To the south you see a beach.\n To the west you see a beach." /*yard?*/ ,new List<string>{ "juice boxes", "exam papers", "pen" });
             Room? museum = new("Museum", "You've entered a museum.\n To the north you see a dumping yard.\n To the east you see... nothing.\n To the south you see a school.\n To the west you see a beach." ,new List<string>{ "coffee cups", "exhibit maps", "cans" });
-            Room? beach = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            
+            Room? beachNorth1 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachNorth2 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachNorth3 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachEast1 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachEast2 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachEast3 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachEast4 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachSouth1 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachSouth2 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachSouth3 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachWest1 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachWest2 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachWest3 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachWest4 = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+           
+            Room? beachNorthEast = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachSouthEast = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachSouthWest = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
+            Room? beachNorthWest = new("Beach", "You've entered a beach." ,new List<string>{ "plastic bottles", "fishing nets", "clothing", "flip-flops", "straw", "sand toy", "beach ball", "sunscreen bottles", "popped inflatable rafts", "abandoned beach towels", "plastic wraps and boxes", "cigarette butts" });
 
 
             hallway.SetExits(bathroom, cityCenter, null, kitchen); // North, East, South, West
@@ -50,27 +69,51 @@ namespace WorldOfZuul
             kitchen.SetExits(bedroom, hallway, livingRoom, null);
             livingRoom.SetExits(kitchen, null, null, null);
 
-            cityCenter.SetExits(factory, beach, mall, hallway);
+            cityCenter.SetExits(factory, beachEast3, mall, hallway);
 
-            factory.SetExits(forest, beach, cityCenter, pond);
+            factory.SetExits(forest, beachEast2, cityCenter, pond);
 
-            forest.SetExits(beach, beach, factory, waterfall);
+            forest.SetExits(beachNorth3, beachEast1, factory, waterfall);
 
-            waterfall.SetExits(beach, forest, pond, clearing);
+            waterfall.SetExits(beachNorth2, forest, pond, clearing);
 
-            clearing.SetExits(beach, waterfall, dumpingYard, beach);
+            clearing.SetExits(beachNorth1, waterfall, dumpingYard, beachWest1);
 
             pond.SetExits(waterfall, factory, null, dumpingYard);
 
-            dumpingYard.SetExits(clearing, pond, museum, beach);
+            dumpingYard.SetExits(clearing, pond, museum, beachWest2);
 
-            museum.SetExits(dumpingYard, null, school, beach);
+            museum.SetExits(dumpingYard, null, school, beachWest3);
 
-            school.SetExits(museum, park, beach, beach);
+            school.SetExits(museum, park, beachWest4, beachSouth1);
 
-            park.SetExits(null, mall, beach, school);
+            park.SetExits(null, mall, beachSouth2, school);
 
-            mall.SetExits(cityCenter, beach, beach, park);
+            mall.SetExits(cityCenter, beachEast4, beachSouth3, park);
+
+            beachNorth1.SetExits(null, beachNorth2, clearing, beachNorthWest);
+            beachNorth2.SetExits(null, beachNorth3, waterfall, beachNorth1);
+            beachNorth3.SetExits(null, beachNorthEast, forest, beachNorth2);
+
+            beachEast1.SetExits(beachNorthEast, null, beachEast2, forest);
+            beachEast2.SetExits(beachEast1, null, beachEast3, factory);
+            beachEast3.SetExits(beachEast2, null, beachEast4, cityCenter);
+            beachEast4.SetExits(beachEast3, null, beachSouthEast, mall);
+
+            beachSouth1.SetExits(school, beachSouth2, null, beachSouthWest);
+            beachSouth2.SetExits(park, beachSouth3, null, beachSouth1);
+            beachSouth3.SetExits(mall, beachSouthEast, null, beachSouth2);
+
+            beachWest1.SetExits(beachNorthWest, clearing, beachWest2, null);
+            beachWest2.SetExits(beachWest1, dumpingYard, beachWest3, null);
+            beachWest3.SetExits(beachWest2, museum, beachWest4, null);
+            beachWest4.SetExits(beachWest3, school, beachSouthWest, null);
+
+            beachNorthWest.SetExits(null, beachNorth1, beachWest1, null);
+            beachNorthEast.SetExits(null, null, beachEast1, beachNorth3);
+            beachSouthEast.SetExits(beachEast4, null, null, beachSouth3);
+            beachSouthWest.SetExits(beachEast4, beachSouth1, null, null);
+
             currentRoom = hallway;
         }
         public void Play()
@@ -151,7 +194,7 @@ namespace WorldOfZuul
                     case "inventory":
                         inventory.ShowInventory();
                         break;
-                        
+
                     // case "take":
                     //     TakeItem(command.Name, 0);
 
