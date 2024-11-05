c9e6783f36f5f5099bb5d7765b61d6a08949bcce
dskorepa04 <dave.skorepa@gmail.com>
2024-11-05 13:02:46 +0100

    Update Game.cs

--- WorldOfZuul/Game.cs
+++ WorldOfZuul/Game.cs
@@ -233,25 +233,26 @@ namespace WasteHunters
 
         public void PrintMap()
         {
-            Console.WriteLine("         ╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗");
-            Console.WriteLine("         ║    BEACH     ║║    BEACH     ║║    BEACH     ║║    BEACH     ║║    BEACH     ║");
-            Console.WriteLine("         ╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝");
-            Console.WriteLine("         ╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗");
-            Console.WriteLine("         ║    BEACH     ║║   CLEARING   ║║  WATERFALL   ║║    FOREST    ║║    BEACH     ║");
-            Console.WriteLine("         ╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝");
-            Console.WriteLine("         ╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗");
-            Console.WriteLine("         ║    BEACH     ║║ DUMPING YARD ║║     POND     ║║    FACTORY   ║║    BEACH     ║");
-            Console.WriteLine("         ╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝");
-            Console.WriteLine("         ╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗");
-            Console.WriteLine("         ║    BEACH     ║║    MUSEUM    ║║   KITCHEN    ║║   HALLWAY    ║║  CITY CENTER ║║    BEACH     ║");
-            Console.WriteLine("         ╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝");
-            Console.WriteLine("         ╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗");
-            Console.WriteLine("         ║    BEACH     ║║    SCHOOL    ║║     PARK     ║║     MALL     ║║    BEACH     ║");
-            Console.WriteLine("         ╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝");
-            Console.WriteLine("         ╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗╔══════════════╗");
-            Console.WriteLine("         ║    BEACH     ║║    BEACH     ║║    BEACH     ║║    BEACH     ║║    BEACH     ║");
-            Console.WriteLine("         ╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝╚══════════════╝");
-        }
+            Console.WriteLine("         ╔═══════╗     ╔═══════╗            ╔═══════╗            ╔═══════╗      ╔═══════╗");
+            Console.WriteLine("         ║ BEACH ║═════║ BEACH ║════════════║ BEACH ║════════════║ BEACH ║══════║ BEACH ║");
+            Console.WriteLine("         ╔═══║═══╗ ╔══════║═══════╗      ╔══════║═══════╗      ╔══════║═══════╗ ╔═══║═══╗");
+            Console.WriteLine("         ║ BEACH ║═║   CLEARING   ║══════║   WATERFALL  ║══════║    FOREST    ║═║ BEACH ║");
+            Console.WriteLine("         ╚═══║═══╝ ╚══════║═══════╝      ╚══════║═══════╝      ╚══════║═══════╝ ╚═══║═══╝");
+            Console.WriteLine("         ╔═══║═══╗ ╔══════║═══════╗      ╔══════║═══════╗      ╔══════║═══════╗ ╔═══║═══╗");
+            Console.WriteLine("         ║ BEACH ║═║ DUMPING YARD ║══════║     POND     ║══════║   FACTORY    ║═║ BEACH ║");
+            Console.WriteLine("         ╚═══║═══╝ ╚══════║═══════╝      ╚══════════════╝      ╚══════║═══════╝ ╚═══║═══╝");
+            Console.WriteLine("             ║            ║         ╔════════════════════════╗        ║             ║    ");
+            Console.WriteLine("         ╔═══║═══╗ ╔══════║═══════╗ ║ Bedroom      Bathroom  ║ ╔══════║═══════╗ ╔═══║═══╗");
+            Console.WriteLine("         ║ Beach ║═║  RESTAURANT  ║ ║ Kitchen      Hallway   ║═║  CITY CENTER ║═║ BEACH ║");
+            Console.WriteLine("         ╚═══║═══╝ ╚══════║═══════╝ ║ Living room            ║ ╚══════║═══════╝ ╚═══║═══╝");
+            Console.WriteLine("             ║            ║         ╚════════════════════════╝        ║             ║    ");
+            Console.WriteLine("         ╔═══║═══╗ ╔══════║═══════╗      ╔══════════════╗      ╔══════║═══════╗ ╔═══║═══╗");
+            Console.WriteLine("         ║ BEACH ║═║ GROCERY STORE║══════║     PARK     ║══════║    MALL      ║═║ BEACH ║");
+            Console.WriteLine("         ╚═══║═══╝ ╚══════║═══════╝      ╚══════║═══════╝      ╚══════║═══════╝ ╚═══║═══╝");
+            Console.WriteLine("         ║ BEACH ║═════║ BEACH ║════════════║ BEACH ║════════════║ BEACH ║══════║ BEACH ║");
+            Console.WriteLine("         ╚═══════╝     ╚═══════╝            ╚═══════╝            ╚═══════╝      ╚═══════╝");
+
+         }
         private static void PrintWelcome()
         {
             Console.WriteLine("Welcome to the Waste Hunters game!");
